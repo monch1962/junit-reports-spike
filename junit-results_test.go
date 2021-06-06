180676d0008f9731613017f685249d0d06913242
David Mitchell <monch1962@gmail.com>
2021-06-06 10:42:57 +0000

    More test cases

--- junit-results_test.go
+++ junit-results_test.go
@@ -6,15 +6,19 @@ import (
 	"github.com/joshdk/go-junit"
 )
 
-func TestJunitFileIngest(t *testing.T) {
-	suites, err := junit.IngestFiles([]string{
-		"junit-results-files/java-results-1.xml",
-	})
+func TestJunitSingleFileIngest(t *testing.T) {
+	suites, err := junit.IngestFile("junit-results-files/java-results-1.xml")
+	if err != nil {
+		t.Fatalf("failed to ingest JUnit xml %v", err)
+	}
 	if err != nil {
 		t.Fatalf("failed to ingest JUnit xml %v", err)
 	}
 	//fmt.Printf("Suites ingested: %v", suites)
-	t.Log(suites)
+	//t.Log(suites)
+	if len(suites) != 2 {
+		t.Fatalf("Expected 2 suites, found %d", len(suites))
+	}
 
 	for _, suite := range suites {
 		t.Logf("Suite: %s\n", suite.Name)
@@ -28,4 +32,53 @@ func TestJunitFileIngest(t *testing.T) {
 		}
 	}
 
+}
+func TestJunitMultipleFilesIngest(t *testing.T) {
+	suites, err := junit.IngestFiles([]string{
+		"junit-results-files/java-results-1.xml",
+	})
+	if err != nil {
+		t.Fatalf("failed to ingest JUnit xml %v", err)
+	}
+	//fmt.Printf("Suites ingested: %v", suites)
+	//t.Log(suites)
+	if len(suites) != 2 {
+		t.Fatalf("Expected 2 suites, found %d", len(suites))
+	}
+
+	for _, suite := range suites {
+		t.Logf("Suite: %s\n", suite.Name)
+		for _, test := range suite.Tests {
+			t.Logf("  TestName: %s\n", test.Name)
+			if test.Error != nil {
+				t.Logf("    Test result: %s: %s\n", test.Status, test.Error.Error())
+			} else {
+				t.Logf("    Test result: %s\n", test.Status)
+			}
+		}
+	}
+
+}
+
+func TestJunitResultsDirectoryIngest(t *testing.T) {
+	suites, err := junit.IngestDir("junit-results-files/")
+	if err != nil {
+		t.Fatalf("failed to ingest JUnit xml %v", err)
+	}
+	//t.Log(suites)
+	if len(suites) != 4 {
+		t.Fatalf("Expected 4 suites, found %d", len(suites))
+	}
+
+	for _, suite := range suites {
+		t.Logf("Suite: %s\n", suite.Name)
+		for _, test := range suite.Tests {
+			t.Logf("  TestName: %s\n", test.Name)
+			if test.Error != nil {
+				t.Logf("    Test result: %s: %s\n", test.Status, test.Error.Error())
+			} else {
+				t.Logf("    Test result: %s\n", test.Status)
+			}
+		}
+	}
 }
